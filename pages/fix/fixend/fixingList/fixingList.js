b62995e911ec520c5ec77ca83ab45672b106ee27
unknown <unknown@example.com>
2019-11-23 10:39:22 +0800

    201911231039

--- pages/fix/fixend/fixingList/fixingList.js
+++ pages/fix/fixend/fixingList/fixingList.js
@@ -1,6 +1,6 @@
 // pages/fix/fixend/fixingList/fixingList.js
-// var axFixing = require('../../../ax/fix/fixing.js');
-var axFixEnd = require('../../../ax/fix/fixend.js');
+var axFixing = require('../../../ax/fix/fixing.js');
+// var axFixEnd = require('../../../ax/fix/fixend.js');
 var tool = require('../../../tools/tool.js');
 Component({
   /**
@@ -29,7 +29,8 @@ Component({
       wx.showLoading({
         title: 'waiting',
       })
-      axFixEnd.getAllList().then(function (data) {
+      // 不是查询已经结束的列表，是查询待处理为结束，所以为fixing
+      axFixing.getAllList().then(function (data) {
         wx.hideLoading();
         if (tool.chkRes(data)) return;
         console.log(data);
