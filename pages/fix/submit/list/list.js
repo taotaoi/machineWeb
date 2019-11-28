e1b7d592912db4322961062798b7c295691e38af
WL <taotaov@qq.com>
2019-11-28 16:54:06 +0800

    201911281654

--- pages/fix/submit/list/list.js
+++ pages/fix/submit/list/list.js
@@ -13,7 +13,10 @@ Component({
    * 组件的初始数据
    */
   data: {
-    list:[]
+    list:[],
+    tmp:[],
+    user: {},
+    searchShow: false
   },
 
   /**
@@ -33,6 +36,7 @@ Component({
         wx.hideLoading();
         if(tool.chkRes(data)) return;
         that.setData({
+          tmp: data.data.data,
           list:data.data.data
         })
       });
@@ -41,6 +45,41 @@ Component({
       var fixid = par.currentTarget.dataset.data;
       // console.log(fixid);
       tool.route("../show/show?id="+fixid);
+    },
+    searchReq(e) {
+      var tmp = e.detail;
+      console.log(tmp);
+      this.setData({ list: [] })
+      
+      // 1、日期筛选
+      if (tmp.date1 != "0000-00-00" && tmp.date2 != "0000-00-00") {
+        console.log("select date on");
+        var tmpArr = [];
+        for (let e of this.data.list) {
+          // 起始日期与e相同 或 结束日期与e相同
+          var t1 = tmp.date1.substring(0, 10);
+          var t2 = tmp.date2.substring(0, 10);
+          var d1 = e.submitdatetime.substring(0, 10);
+          console.log(tmp.date1.substring(0, 10));
+          console.log(e.submitdatetime.substring(0, 10));
+          if ((d1 == t1) || (d1 == t2)) {
+            tmpArr.push(e);
+          }
+          // 如果有在范围内的e push
+          if (tool.dateMax(tmp.date1, e.submitdatetime) && tool.dateMin(tmp.date2, e.submitdatetime)) {
+            // console.log(e);
+            tmpArr.push(e);
+          }
+        }
+        this.setData({ list: tmpArr })
+      }
+      // 2、关键字筛选
+
+    },
+    showMenu() {
+      this.setData({
+        searchShow: true
+      })
     },
   }
 })
